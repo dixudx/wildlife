6ccaf80a01e92d813ef1fbfebda74e5093ca8f88
stephenhsu <stephenhsu90@gmail.com>
2015-11-30 21:22:16 +0800

    add decorator to handle exception

--- wildlife/rest.py
+++ wildlife/rest.py
@@ -1,8 +1,11 @@
 from wildlife import WildApp
 import os
-from flask import jsonify, make_response, Response
+from flask import jsonify, make_response
 from wildlife import kz_exceptions
 import logging
+import json
+import exceptions
+import functools
 
 
 # change to current directory
@@ -13,9 +16,35 @@ app = WildApp("wildlife for zookeeper",
               conf_path)
 
 
+def cluster_znode_exception(func):
+    @functools.wraps(func)
+    def wrapper(cluster_name, znode):
+        try:
+            func(cluster_name, znode)
+        except (kz_exceptions.ConnectionClosedError,
+                kz_exceptions.ConnectionDropped,
+                kz_exceptions.ConnectionLoss,
+                kz_exceptions.ConnectionLossException):
+                return make_response("Connection Exception When Interacts "
+                                     "with Cluster %s.\n" % cluster_name,
+                                     408)
+        except kz_exceptions.NoNodeError:
+            return make_response("Cannot Find Znode %s in Cluster"
+                                 "%s.\n" % (znode, cluster_name),
+                                 404)
+        except kz_exceptions.InvalidACLException:
+            return make_response("Invalid ACLs on Accessing Znode %s in "
+                                 "Cluster %s.\n" % (znode, cluster_name),
+                                 401)
+        except exceptions:
+            return make_response("Unable to Handle this Request.\n",
+                                 500)
+    return wrapper
+
+
 @app.route("/")
 def hello():
-    return make_response("Welcome to WildLife: The REST API for ZooKeeper!",
+    return make_response("Welcome to WildLife: The REST API for ZooKeeper!\n",
                          200)
 
 
@@ -32,35 +61,28 @@ def detail_cluster(cluster_name):
 
 
 @app.route("/wildlife/<cluster_name>/<znode>", methods=["GET"])
+@cluster_znode_exception
 def cluster_znode(cluster_name, znode):
     _zclient_manager = app.managers[cluster_name]
     _zclient = _zclient_manager._client
-    try:
-        zdata = _zclient.get(znode)
-        return make_response(jsonify({"data": zdata[0],
-                                      "znodeStat": convert_zstat(zdata[1])
-                                      }),
-                             200)
-    except kz_exceptions.NoNodeError:
-        return make_response("Cannot Find Znode %s in Cluster"
-                             "%s.\n" % (znode, cluster_name),
-                             404)
+    zdata = _zclient.get(znode)
+    return make_response(jsonify({"data": zdata[0],
+                                  "znodeStat": convert_zstat(zdata[1])
+                                  }),
+                         200)
 
 
 @app.route("/wildlife/<cluster_name>/<znode>/data", methods=["GET"])
+@cluster_znode_exception
 def cluster_znode_data(cluster_name, znode):
-    _zclient_manager = app.managers[cluster_name]
-    _zclient = _zclient_manager._client
-    print znode
-    try:
-        zdata = _zclient.get(znode)
-        return make_response(zdata[0],
-                             200)
-    except:
-        pass
+    zdata = cluster_znode(cluster_name, znode)
+    zdata = json.loads(zdata)
+    return make_response(zdata["data"],
+                         200)
 
 
 @app.route("/wildlife/<cluster_name>/<znode>/children", methods=["GET"])
+@cluster_znode_exception
 def cluster_znode_children(cluster_name, znode):
     _zclient_manager = app.managers[cluster_name]
     _zclient = _zclient_manager._client
@@ -69,18 +91,18 @@ def cluster_znode_children(cluster_name, znode):
                          200)
 
 
-def convert_zstat(znodeStat):
-    return {"czxid": znodeStat.czxid,
-            "mzxid": znodeStat.mzxid,
-            "ctime": znodeStat.ctime,
-            "mtime": znodeStat.mtime,
-            "version": znodeStat.version,
-            "cversion": znodeStat.cversion,
-            "aversion": znodeStat.aversion,
-            "ephemeralOwner": znodeStat.ephemeralOwner,
-            "dataLength": znodeStat.dataLength,
-            "numChildren": znodeStat.numChildren,
-            "pzxid": znodeStat.pzxid}
+def convert_zstat(znodestat):
+    return {"czxid": znodestat.czxid,
+            "mzxid": znodestat.mzxid,
+            "ctime": znodestat.ctime,
+            "mtime": znodestat.mtime,
+            "version": znodestat.version,
+            "cversion": znodestat.cversion,
+            "aversion": znodestat.aversion,
+            "ephemeralOwner": znodestat.ephemeralOwner,
+            "dataLength": znodestat.dataLength,
+            "numChildren": znodestat.numChildren,
+            "pzxid": znodestat.pzxid}
 
 
 if __name__ == "__main__":
